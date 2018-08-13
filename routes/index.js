fc969023a244318f4c49fba7d88fecb47dc8b602
iamrajajamal <martian.hacked@gmail.com>
2018-08-13 15:49:55 +0500

    upload with github

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 // Do work here
-router.get('/',storeController.myMiddleware, storeController.homePage)
+router.get('/', storeController.homePage)
 
 module.exports = router;
