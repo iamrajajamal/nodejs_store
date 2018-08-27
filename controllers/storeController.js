3aba8c6210d678e1ad7e2d6385903d24f6074b5e
iamrajajamal <martian.hacked@gmail.com>
2018-08-27 15:03:37 +0500

    Implement a edit store page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -26,9 +26,20 @@ exports.getStores = async (req, res) => {
 
 exports.editStore = async (req, res) => {
     // 1. Find the store given the ID
-    const store = await Store.findOne({ _id: req.params._id });
-    // 2. confirm they are the owner of the store
-    // TODO:
+    const store = await Store.findOne({ _id: req.params.id });
+    // TODO: 2. confirm they are the owner of the store
+    // 
     // 3. Render out the edit form so the user can update their store
-    
+    res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res) => {
+    // find and update the store
+    const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+        new: true, // return the new store instead of the old one
+        runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <ahref="/stores/${store.slug}">View Store</a>`);
+    res.redirect(`/store/${store._id}/edit`);
+    // Redirect then the store and tell them it worked
 }
