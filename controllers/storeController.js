272101a074bfe6850794d48ac17d678d11a66ae7
iamrajajamal <martian.hacked@gmail.com>
2019-01-14 18:51:02 +0500

    show stores in tags and filter stores by tags'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -89,8 +89,9 @@ exports.getStoreBySlug = async (req, res) => {
 
 exports.getStoreByTag = async (req, res) => {
     const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true };
     const tagsPromise = Store.getTagsList();
-    const storesPromise = Store.find({ tags: tag });
+    const storesPromise = Store.find({ tags: tagQuery });
     const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
     res.render('tags',{tags, title: 'Tags', tag, stores});
 }
