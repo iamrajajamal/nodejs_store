81a7504ee2595d7d9b6e7385c252eff34f4de7a0
iamrajajamal <martian.hacked@gmail.com>
2019-01-15 17:02:32 +0500

    registering a user and, authController and make a handler for passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const User = mongoose.model("User");
+const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
     res.render('login', { title: 'Login' });
@@ -13,7 +15,7 @@ exports.validateRegister = (req, res, next) => {
     req.checkBody('name', 'You must supply a name!').notEmpty();
     req.checkBody('email', 'That Email is not valid!').isEmail();
     req.sanitizeBody('email').normalizeEmail({
-        remove_dots: false,
+        gmail_remove_dots: false,
         remove_extension: false,
         gmail_remove_subaddress: false
     });
@@ -29,3 +31,10 @@ exports.validateRegister = (req, res, next) => {
     }
     next(); // there were no errors!
 };
+
+exports.register = async(req, res, next) => {
+    const user = new User({ email: req.body.email, name: req.body.name });
+    const register = promisify(User.register, User);
+    await register(user, req.body.password);
+    next();
+}
